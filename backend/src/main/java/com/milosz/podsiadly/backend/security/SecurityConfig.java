7f32ea9539cdb4cc63259ac33e6e42d168ec39b7
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-08-27 17:14:18 +0200

    Disable csrf for /api**

--- backend/src/main/java/com/milosz/podsiadly/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/milosz/podsiadly/backend/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/logout"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/logout", "/api/**"))
                 .cors(Customizer.withDefaults())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(auth -> auth
